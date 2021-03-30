5f17131c598a7254e87ac332a5ea488040e97fbc
Leo <leolazzarini101@gmail.com>
2021-03-30 15:46:37 -0700

    Homepage and task form created

---
+++ ToDoList/Controllers/TasksController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using ToDoList.Models;
+
+namespace ToDoList.Controllers
+{
+    public class TasksController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult New()
+        {
+            var Task = new Task();
+            
+           return View("TaskForm", Task);
+        }
+    }
+}
---
+++ ToDoList/Models/Task.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoList.Models
+{
+    public class Task
+    {
+        public string Title { get; set; }
+        [Display(Name = "Task Details")]
+        public string Body { get; set; }
+        public DateTime CreatedDate { get; set; }
+        [Display(Name = "Deadline")]
+        public DateTime EndDate { get; set; }
+        public DateTime ETA { get; set; }
+    }
+}
