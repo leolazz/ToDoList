b1c41cba96dc685cbf0da2d6c1d2658b7b7c0a40
Leo <leolazzarini101@gmail.com>
2021-05-28 10:34:09 -0700

    Projects class and view added. Redudant Code removed.

---
+++ Models/Project.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoList.Models
+{
+    public class Project
+    {
+        public string UserId { get; set; }
+        public virtual ApplicationUser User { get; set; }
+        public List<Task> Tasks { get; set; }
+    }
+}
