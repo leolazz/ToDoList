4314978f314b193ea14c49313ddb8d527a55d336
Leo <leolazzarini101@gmail.com>
2021-07-06 14:53:28 -0700

    Directory changed for tests, database diagram added, blank lines removed

--- Migrations/SQLiteDBContext.cs
+++ Migrations/SQLiteDBContext.cs
@@ -11,21 +11,21 @@ namespace ToDoList.Data
     
         public class SQLiteDBContext : IdentityDbContext<ApplicationUser>
         {
-          //  public DbSet<Project> Projects { get; set; }
             public DbSet<Task> Tasks { get; set; }
             public DbSet<TaskDetails> Details { get; set; }
             public DbSet<TaskOutcomes> Outcomes { get; set; }
             public DbSet<TaskQualifiers> Qualifiers { get; set; }
             public DbSet<ApplicationUser> ApplicationUsers { get; set; }
             public DbSet<Project> Projects { get; set; }
-        protected override void OnConfiguring(DbContextOptionsBuilder options) 
+            protected override void OnConfiguring(DbContextOptionsBuilder options) 
                 => options.UseSqlite("Data Source=DB/sqlitetodolist.db");
-            public SQLiteDBContext(DbContextOptions<SQLiteDBContext> options)
-            : base(options)
-            {
-            }
+
+        public SQLiteDBContext(DbContextOptions<SQLiteDBContext> options)
+        : base(options)
+        {
+        } 
 
         }
         
 
-    }
+}
