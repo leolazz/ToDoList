d1cd98eb4c9d3970ef787b899466a1a412f0110b
Leo <leolazzarini101@gmail.com>
2021-07-08 13:41:11 -0700

    Working test with Sqlite in memory DB

---
+++ tests/TestDbContext.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using ToDoList.Models;
+
+namespace ToDoList.Tests
+{
+    public class SampleDbContextFactory : IDisposable
+    {
+        private DbConnection _connection;
+        private DbContextOptions<TestSQLiteDBContext> CreateOptions()
+        {
+            return new DbContextOptionsBuilder<TestSQLiteDBContext>()
+                .UseSqlite(_connection).Options;
+        }
+        public TestSQLiteDBContext CreateContext()
+        {
+            if (_connection == null)
+            {
+                _connection = new SqliteConnection("DataSource=:memory:");
+                _connection.Open();
+
+                var options = CreateOptions();
+                using (var context = new TestSQLiteDBContext(options))
+                {
+                    context.Database.EnsureCreated();
+                }
+            }
+            return new TestSQLiteDBContext(CreateOptions());
+        }
+
+        public void Dispose()
+        {
+            if (_connection != null)
+            {
+                _connection.Dispose();
+                _connection = null;
+            }
+        }
+
+        public class TestSQLiteDBContext : IdentityDbContext<ApplicationUser>
+        {
+            public TestSQLiteDBContext(DbContextOptions options)
+            : base(options)
+            {
+            }
+            public TestSQLiteDBContext()
+            {
+            }
+            public DbSet<Task> Tasks { get; set; }
+            public DbSet<TaskDetails> Details { get; set; }
+            public DbSet<TaskOutcomes> Outcomes { get; set; }
+            public DbSet<TaskQualifiers> Qualifiers { get; set; }
+            public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+            public DbSet<Project> Projects { get; set; }
+
+        }
+    }
+}
+
--- tests/UnitTest1.cs
+++ tests/UnitTest1.cs
@@ -1,6 +1,8 @@
 using System;
 using Xunit;
 using ToDoList.Services;
+using ToDoList.Models;
+using System.Linq;
 
 namespace ToDoList.Tests
 {
@@ -9,7 +11,32 @@ namespace ToDoList.Tests
         [Fact]
         public void NewTask_Returns_New_TaskDto()
         {
+            using (var factory = new SampleDbContextFactory())
+            {
+                using (var context = factory.CreateContext())
+                {
+                    var task = new Task() 
+                    {
+                        CreatedDate = DateTime.Now,
+                        Title = "test",
+                        Details = new TaskDetails() { Details = "test" },
+                        Qualifiers = new TaskQualifiers() { Qualifiers = "test" },
+                        Outcomes = new TaskOutcomes() { Outcomes = "test" },
+                        Completed = false,
+                        EndDate = DateTime.Now.AddDays(1),
+                    };
+                    context.Tasks.Add(task);
+                    context.SaveChanges();
+                }
+                using (var context = factory.CreateContext())
+                {
+                    var count = context.Tasks.Count();
+                    Assert.Equal(0, count);
 
+                    var t = context.Tasks.FirstOrDefault(task => task.Title == "test");
+                    Assert.NotNull(t);
+                }
+            }
         }
     }
 }
